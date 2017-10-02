ac66e45c2fb45bd10b938e6153054144b3dd3ce9
James Kropp <james-kropp@hotmail.com>
2017-10-02 09:56:49 -0700

    Generated Knock methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  post 'user_token' => 'user_token#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
