e3f1f437da956a00db8823310d96361994fad24a
James Kropp <james-kropp@hotmail.com>
2017-10-02 10:45:34 -0700

    Added user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+  
+  has_secure_password
+  
+  validates_length_of       :password, maximum: 72, minimum: 8, allow_nil: true, allow_blank: false
+  validates_confirmation_of :password, allow_nil: true, allow_blank: false
+
+  before_validation { 
+    (self.email = self.email.to_s.downcase) && (self.username = self.username.to_s.downcase) 
+  }
+
+  validates_presence_of     :email
+  validates_presence_of     :username
+  validates_uniqueness_of   :email
+  validates_uniqueness_of   :username
+
 end
