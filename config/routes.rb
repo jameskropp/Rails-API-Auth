d86a0a18942552ec8f801971f7cf20336e42d538
James Kropp <james-kropp@hotmail.com>
2017-10-20 11:44:00 -0700

    Format the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+  
+  # Home controller routes.
   root   'home#index'
   get    'auth'            => 'home#auth'
   
+  # Get login token from Knock
   post   'user_token'      => 'user_token#create'
   
-  get    'users'           => 'users#index'
+  # User actions
+  get    '/users'          => 'users#index'
   get    '/users/current'  => 'users#current'
   post   '/users/create'   => 'users#create'
   patch  '/user/:id'       => 'users#update'
   delete '/user/:id'       => 'users#destroy'
+  
 end
